395950e53d964d3e88d38eab79e7a8736494bb8d
jamesaldridge <jamesaldridge711@gmail.com>
2018-11-08 00:19:51 +0000

    Initial Commit

---
+++ GameSFML/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameSFML
+//
+//  Created by James Aldridge on 08/11/2018.
+//  Copyright © 2018 James Aldridge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
